22ac7c674e8ae9764a77dba1ba9fd6a47897acba
Arsh Natani <arshnatani123456@gmail.com>
2025-12-13 00:02:49 +0530

    resolved error in vedorProduct Routes

--- routers/vendorProductRoutes.js
+++ routers/vendorProductRoutes.js
@@ -17,11 +17,12 @@ router.use(protect);
 router.post("/", addProduct); // POST /api/vendor/product
 router.get("/", getMyProducts); // GET  /api/vendor/product
 
+// 3. Read all the product listed
+router.get("/all", getAllProducts);
+
 // 2. Read, Update, Delete Single Items
 router.get("/:id", getProductById); // GET    /api/vendor/product/:id
 router.put("/:id", updateProduct); // PUT    /api/vendor/product/:id
 router.delete("/:id", deleteProduct); // DELETE /api/vendor/product/:id
 
-// 3. Read all the product listed
-router.get("/all",getAllProducts)
 module.exports = router;
